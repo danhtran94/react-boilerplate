71e2e2ab45111b997c17d3bc5e3dac529a0a6e6c
Danh Tran <dd2672015@gmail.com>
2016-12-16 23:37:32 +0700

    yooo

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import style from "../styles/main.scss";
 
 ReactDOM.render(<div className="example">
-	<h1 className="title">Hello 2 sdfaf World</h1>
+	<h1 className="title">Welcome To My Boilerplate</h1>
 </div>, document.getElementById('example'));
